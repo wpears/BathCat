57671fc0a4c4619ab68550f1792fcbf22b82452d
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-07-07 11:34:57 -0700

    Formalizing scripts, build, and gruntfile

--- dev/updateServices.js
+++ dev/updateServices.js
@@ -20,8 +20,12 @@ function regStream(chunk, enc, cb){
   cb();
 }
 var server = process.argv[2];
+var dataName = process.argv[3];
+var tightOutName = process.argv[4];
 
 if(!server) throw new Error("Must provide server name as first argument.");
+if(!dataName) throw new Error("Must provide data service name as second argument.");
+if(!tightOutName) throw new Error("Must provide outline service name as third argument.");
 
 run(
     "http://"+server+"/ArcGIS/rest/services/BATH/data_out/MapServer/0/query?f=json&where=1%20%3D%201&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*&outSR=102100"

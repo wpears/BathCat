c51c91a2ad3a995466cf705f96c09ee4b3cb0c1c
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-07-31 16:33:50 -0700

    set tool visibility bug

--- modules/settoolvisibility.js
+++ modules/settoolvisibility.js
@@ -2,11 +2,11 @@ define(["dojo/dom-class","modules/tools.js"],function(domClass,Tools){
     return function (tools, hideTools){
       for(var i=0; i < tools.length; i++){
         if(hideTools){
-          domClass.replace(tools[i].anchor,"clickable","unclick");
+          domClass.replace(tools[i].anchor,"unclick","clickable");
         }else{
           if(tools[i].tool)Tools.wipe(tools[i].tool,tools[i].anchor,tools[i].eventFeatures)
-          domClass.replace(tools[i].anchor,"unclick","clickable");
+          domClass.replace(tools[i].anchor,"clickable","unclick");
         }
       }
     }
-})
+});

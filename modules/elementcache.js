d281f21d1fe99f7ab71732c6e5e4e06c2ed91527
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-08-22 13:58:52 -0700

    caching module documented

--- modules/elementcache.js
+++ modules/elementcache.js
@@ -1,18 +1,20 @@
 define([],function(){
-  return function(element,amount){
+
+  /**Call with an element name to create a simple cache which saves old elements to be reused
+   **Used in the catalog for the hidden imgs and canvases involved in the profile tool's internals
+   **/
+  return function(element){
      var cache=[]
        , pointer = 0
        , doc = document
        ;
 
+
     function get(){
       var el;
-      //console.log("getting from elcache",pointer,cache,cache.length)
       if(pointer === cache.length){
-        //console.log("Equal, creating anew")
         el = doc.createElement(element);
       }else{
-        //console.log("Unequal",cache[pointer],"being returned")
         el = cache[pointer]
         cache[pointer] = null;
         pointer++;
@@ -21,19 +23,19 @@ define([],function(){
           pointer = 0;
         }
       }
-      //console.log("returning",el,pointer,cache,cache.length)
       return el
     }
 
+
     function reclaim(el){
-      //console.log("reclaiming", pointer, cache, cache.length)
       cache[cache.length] = el;
-      //console.log("reclaimed",pointer,cache,cache.length)
     }
+
 
     return {
       get:get,
       reclaim:reclaim
     }
+
   }
 });

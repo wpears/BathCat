9c719ef9cf67c634eff53a2603df573859da8a27
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-08-21 17:41:54 -0700

    Update works, needs css support

--- buildfiles/tasks/update.js
+++ buildfiles/tasks/update.js
@@ -3,20 +3,46 @@ var async = require('async');
 var cheerio = require('cheerio');
 var getBuilds = require('../modules/getBuilds')
 
+
 module.exports = function(grunt){
   grunt.registerTask('update', 'Update build dependencies',function(){
     var done = this.async();
 
-    fs.readFile('index.html',function(err,data){
+    async.map(['./','../dojo/'], getBuilds, function(err,buildsArr){
       if(err){
-        grunt.log.error("\nError reading index.html\n");
+        grunt.log.error("\nError getting builds.\n");
         return done();
       }
+      var bcBuildDir = './' + buildsArr[0].sort().pop();
+      var dojoBuildDir = '../dojo/' + buildsArr[1].sort().pop();
 
-      var $ = cheerio.load(data);
+      var bcSrc = bcBuildDir + '/BathCat.min.js';
+      var dojoSrc = dojoBuildDir + '/dojo/dojo.js';
+
+      fs.readFile('index.html',function(err,data){
+        if(err){
+          grunt.log.error("\nError reading index.html\n");
+          return done();
+        }
+
+        var $ = cheerio.load(data);
+
+        $('#bcScript').attr('src',bcSrc);
+        $('#dojoScript').attr('src',dojoSrc);
 
 
-    })
+        fs.writeFile('index.html',$.html(),function(err){
+          if(err){
+            grunt.log.error("\nError writing html.\n");
+          }
+          grunt.log.writeln("\nindex.html sources updated to latest build.\n");
+          return done();
+        });
+
+      });
+
+    });
 
   });
+
 }

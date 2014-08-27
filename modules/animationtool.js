3f6b888975bb2db898575e689aca5f06fb44430e
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-08-26 18:27:34 -0700

    found proper container

--- modules/animationtool.js
+++ modules/animationtool.js
@@ -1,28 +1,84 @@
 define( ["dojo/on"
+        ,"modules/getrasterurl"
+        ,"modules/elementcache"
         ],
 function( on
+        , GetRasterUrl
+        , ElementCache
         ){
-  return function(rastersShowing, geoSearch, rasterLayer, map){
+  return function(features, rastersShowing, geoSearch, rasterLayer, map){
 
-    var animTargets = getTargets();
-    var playButton = document.getElementById("anim");
+    var getRasterUrl = GetRasterUrl(rasterLayer, map);
+    var imgCache = ElementCache('img');
+
+    var animTargets;
+    var images = {};
+    var DOC = document;
+    var playButton = DOC.getElementById("anim");
+
+    var container;
 
     on(playButton,"mousedown",animate);
 
     function animate(){
+      if(!container) container = DOC.getElementById("mapDiv_layers");
+      animTargets = getTargets();
+      makeImages(animTargets);
+
       var current = animTargets[0];
+      console.log(animTargets)
     }
 
 
 
 
     function getTargets(){
-      return geoSearch.selected.filter(filterTargets)
+      return geoSearch.selected
+              .filter(rastersOn)
+              .map(oidsToLayers)
+              .sort(featureDates)
+              ;
     }
 
-    function filterTargets(target){
+
+    function rastersOn(target){
       return rastersShowing[target]
     }
+
+
+    function oidsToLayers(target){
+      return target - 1;
+    }
+
+
+    function featureDates(a,b){
+      return +features[a].attributes.Date - +features[b].attributes.Date;
+    }
+
+
+    function makeImages(targets){
+      var count = targets.length;
+      targets.forEach(function(v){
+        var image = imgCache.get();
+        image.className = "animationImage";
+        image.onload = function(){
+          if(--count===0){
+            startAnimation();
+          }
+        }
+        image.src=getRasterUrl.getUrl(v);
+        images[v]=image;
+        container.appendChild(image);
+      });
+    }
+
+
+    function startAnimation(){
+      console.log("STARTING");
+      rasterLayer.hide();
+
+    }
+
 
 
 

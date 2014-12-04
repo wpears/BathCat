8d9a05fc0223c6620816680f5d76ebc94e597a8a
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-12-03 16:39:22 -0800

    Beware of pythonpath weirdness on imports

---
+++ buildfiles/zip_client.py
@@ -0,0 +1,6 @@
+#import sys
+#sys.path.append('\\\\mrsbmapp21161\\giswebapps\\bathymetry\\buildfiles')
+
+from zipXYZ import ZipXYZ
+
+ZipXYZ('\\\\mrsbmapp21161\\giswebapps\\bathymetry\\buildfiles\\mbt.py')

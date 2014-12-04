c8587bf00b912479c22f87e7c5e9430728f99b79
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-12-03 16:19:37 -0800

    Zipping script

---
+++ buildfiles/zipXYZ.py
@@ -0,0 +1,9 @@
+from os import path
+from zipfile import ZipFile
+
+def ZipXYZ (xyz, dir='\\\\mrsbmapp21161\\giswebapps\\bathymetry\\zips'):
+
+  file_name = path.basename(xyz)
+
+  with ZipFile(path.join(dir, path.splitext(file_name)[0] + '.zip'),'w') as zippedXYZ:
+    zippedXYZ.write(xyz, file_name)

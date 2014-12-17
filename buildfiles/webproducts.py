4b3bdd1c856153848aabf668b56e5d90095e31d5
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-12-17 10:11:28 -0800

    Adjusted simplify and slight buffer for aesthetics

--- buildfiles/webproducts.py
+++ buildfiles/webproducts.py
@@ -7,7 +7,7 @@ env.outputMFlag="Disabled"
 
 
 
-def WebProducts (raster, mxds, dfs, method="BEND_SIMPLIFY", tolerance=1, minimumArea=3000 ):
+def WebProducts (raster, mxds, dfs, method="POINT_REMOVE", tolerance=3, minimumArea=3000 ):
 
   mxd = mxds[0]
   df = dfs[0]
@@ -15,6 +15,7 @@ def WebProducts (raster, mxds, dfs, method="BEND_SIMPLIFY", tolerance=1, minimum
   tempPath = path.join("in_memory", rastName) + "TEMP"
   temp2Path = tempPath + "2"
   temp3Path = tempPath + "3"
+  temp4Path = tempPath + "4"
 
   print("Running Raster Domain...")
   domain = arcpy.RasterDomain_3d(raster, tempPath, "POLYGON")
@@ -25,18 +26,19 @@ def WebProducts (raster, mxds, dfs, method="BEND_SIMPLIFY", tolerance=1, minimum
   print("Union finished. Running Dissolve...")
   dissolve = arcpy.Dissolve_management(union, temp3Path)
 
-  print("Dissolve finished. Running Simplify...")
-  simp = arcpy.cartography.SimplifyPolygon(dissolve, rastName+"_tight", method, tolerance, minimumArea, "NO_CHECK", "NO_KEEP").getOutput(0)
+  print("Dissolve finished. Running Simplify and slight buffer...")
+  simp = arcpy.cartography.SimplifyPolygon(dissolve, temp4Path, method, tolerance, minimumArea, "NO_CHECK", "NO_KEEP").getOutput(0)
+  tight_buff = arcpy.Buffer_analysis(simp, rastName+"_tight", "10 Feet", "FULL", "", "NONE").getOutput(0)
 
-  print("Simplify finished. Saving to bathymetry_tight_outlines...")
+  print("Tight outline created. Saving to bathymetry_tight_outlines...")
   tight_mxd = mxds[1]
   tight_df = dfs[1]
-  tight_layer = arcpy.mapping.Layer(simp)
+  tight_layer = arcpy.mapping.Layer(tight_buff)
   arcpy.mapping.AddLayer(tight_df, tight_layer)
   tight_mxd.save()
 
 
-  print("Tight outlines saved. Creating buffer...")
+  print("Tight outlines saved. Creating buffer for event outlines...")
   extent = raster.getExtent()
   bufferDistance = (extent.width + extent.height) / 2 / 5
   buff = arcpy.Buffer_analysis(simp, rastName+"_event", str(bufferDistance)+ " Feet", "FULL", "", "NONE").getOutput(0)
@@ -49,16 +51,18 @@ def WebProducts (raster, mxds, dfs, method="BEND_SIMPLIFY", tolerance=1, minimum
   event_mxd.save()
 
   print("Event outlines saved. Removing excess layers...")
-  for layer in arcpy.mapping.ListLayers(mxd)[:5]:
+  for layer in arcpy.mapping.ListLayers(mxd)[:6]:
     arcpy.mapping.RemoveLayer(df, layer)
 
   del tempPath
   del temp2Path
   del temp3Path
+  del temp4Path
   del domain
   del union
   del dissolve
   del simp
+  del tight_buff
   del buff
   del tight_layer
   del event_layer
